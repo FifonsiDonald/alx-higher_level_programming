14f5cfe9f4f6486c9574f10132dfae514d1f2534
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-11-30 18:35:05 -0800

    last try

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
-    arlen = len(sys.argv) - 1
-    if (arlen) == 1:
-        print("{} argument:".format(arlen))
-    elif arlen == 0:
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("{} arguments.".format('0'))
+    elif count == 1:
+        print("{} argument:".format(count))
     else:
-        print("{} arguments.".format(arlen))
-    for i in range(arlen):
+        print("{} arguments:".format(count))
+
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
