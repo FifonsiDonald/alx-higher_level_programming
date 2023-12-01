613f8abc89c7035802389629f57684c9a7128465
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-11-30 18:31:38 -0800

    indentation and parenthesis error fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     import sys
     arlen = len(sys.argv) - 1
     if (arlen) == 1:
-    print("{} argument:".format(arlen)
+        print("{} argument:".format(arlen))
     elif arlen == 0:
-    print("{} arguments.".format('0'))
+        print("{} arguments.".format('0'))
     else:
-    print("{} arguments.".format(arlen)
+        print("{} arguments.".format(arlen))
     for i in range(arlen):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
