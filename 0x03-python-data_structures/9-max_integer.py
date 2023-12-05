a2bf82d84dc1ca38826011cfe25a3b6356606aac
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-12-04 17:51:36 -0800

    pycodestyle fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     great = my_list[0]
-    for x in range(1 ,len(my_list)):
+    for x in range(1, len(my_list)):
         if my_list[x] > great:
             great = my_list[x]
     return (great)
