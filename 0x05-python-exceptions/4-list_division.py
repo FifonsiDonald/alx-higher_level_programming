dc8c6610fd5c97b732e837bf4a77f51864b37e82
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-12-18 16:39:18 -0800

    some more fixes two lines required after an import

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            div = None
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            div = None
+            div = 0
         except IndexError:
             print("out of range")
-            div = None
+            div = 0
         finally:
             division.append(div)
     return division
