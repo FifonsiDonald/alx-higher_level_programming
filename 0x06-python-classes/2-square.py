b2591c6fc23294257616e03b137558eaf74f44a5
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2023-12-19 18:11:16 -0800

    commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Library providing a definition of a class 'Square'
+"""
+
+class Square():
+    """ Declaration of a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Create an instance
+        """
+        if not isinstannce(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Definition of a class 'Square'
+"""
+
+
+class Square():
+    """ Calculate the area of a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Instantiate a 'Square'
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Finds the area of a 'Square'
+        """
+        return self.__size ** 2
+    @property
+    def size(self):
+        """ Getter method for size
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """ Setter method for size
+        """
+        if type(value)is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
