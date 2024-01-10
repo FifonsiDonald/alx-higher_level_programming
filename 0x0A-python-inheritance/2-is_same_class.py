36291f53cebab01fa9bc0c9ba1011536c5234fd7
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-01-09 16:27:05 -0800

    test commit

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class defining function """
+
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the specified class """
+    return (type(obj) == a_class)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+
+class BaseGeometry:
+    """Holds our BaseGeometry """
+
+    def area(self):
+        """It is not implemented """
+        raise Exception("area() is not implemented")
